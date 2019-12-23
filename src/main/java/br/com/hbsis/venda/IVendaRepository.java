63afcbf6be1433c9b12c64f55ec604724ddc5848
LucaBLobe <lucablobe@gmail.com>
2019-12-23 07:55:51 -0300

    Mudança na forma de formatar o codigo de categoria

--- src/main/java/br/com/hbsis/venda/IVendaRepository.java
+++ src/main/java/br/com/hbsis/venda/IVendaRepository.java
@@ -1,13 +1,16 @@
 package br.com.hbsis.venda;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface IVendaRepository extends JpaRepository<Venda, Long> {
 
     Optional<Venda> findFornecedorById(Long fornecedorId);
+
 
 }
